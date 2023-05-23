c268cd9353c297fb54001f0cfeb6a62fc209120b
Sol <solpiq@hotmail.com>
2023-05-23 12:28:00 +0200

    cambios rama sol

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Personajes from './component/Personajes';
 
-function App() {
+
+const RickAndMortyAPI = () => {
+  
+
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Personajes/>
     </div>
   );
-}
+};
 
-export default App;
+export default RickAndMortyAPI;
