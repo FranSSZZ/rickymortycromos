1b71b1fa35277773c89acd8c878b98ea0898d9e7
FranSSZZ <francixix@gmail.com>
2023-05-23 15:15:47 +0200

    denuevootravez

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Mockup from './component/Mockup';
+
+function App() {
+  return (
+    <div className="App">
+      <Mockup/>    
+    </div>
+  );
+}
+
+export default App;
