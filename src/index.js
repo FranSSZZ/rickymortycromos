105a8d01b88654a47ce96c516a0421d08313e643
Santi <bry13041960@gmail.com>
2023-05-23 14:21:03 +0200

    carrusel

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Carrusel from './Carrusel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Carrusel />
   </React.StrictMode>
 );
 
