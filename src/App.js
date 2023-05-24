c0415377f92fc9b9979b6cb1c9c8d7261d92cc0f
FranSSZZ <francixix@gmail.com>
2023-05-24 15:24:17 +0200

    actualizacion

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Mockup from './component/Mockup';
 
 function App() {
   return (
-    <div className="App">
+    <div className="page-container">
+    <div className="content-wrap"></div>
       <Mockup/>    
     </div>
   );
